d0880886ae40ff494652264b3e35f51996b2f6fd
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-04-01 10:15:40 +0200

    here we go

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,13 +1,13 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+  // await expect(wait(input)).rejects.toThrow('milliseconds not a number')
 })
 
+/*
 test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
@@ -15,10 +15,12 @@ test('wait 500 ms', async () => {
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
 })
+*/
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_TOKEN'] = '500'
+  process.env['INPUT_TAG'] = '500'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
